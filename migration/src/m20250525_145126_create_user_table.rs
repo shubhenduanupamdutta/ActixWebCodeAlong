1c30e2bda40eb197a69035ff4e07854c5d8ceae4
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-05-25 21:22:35 +0530

    fix: pre-commit

--- migration/src/m20250525_145126_create_user_table.rs
+++ migration/src/m20250525_145126_create_user_table.rs
@@ -6,7 +6,6 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-
         manager
             .create_table(
                 Table::create()
