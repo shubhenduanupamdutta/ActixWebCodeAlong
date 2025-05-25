3e65a9f2b1fd6ac016e64a0d7b62d21c50797b45
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-05-25 14:27:45 +0530

    Adding a prefix to some routes

--- src/routes/home_routes.rs
+++ src/routes/home_routes.rs
@@ -3,7 +3,9 @@ use actix_web::web;
 use super::handlers::home_handlers;
 
 pub fn config(config: &mut web::ServiceConfig) {
-    config
-        .service(home_handlers::greet)
-        .service(home_handlers::test);
+    config.service(
+        web::scope("/home")
+            .service(home_handlers::greet)
+            .service(home_handlers::test),
+    );
 }
