955074cecf035cd2d3b28b94740eca292c6adc7d
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-06-01 10:12:17 +0530

    Configuring Auth Routes

---
+++ src/routes/auth_routes.rs
@@ -0,0 +1,9 @@
+use actix_web::web;
+
+use super::handlers::home_handlers;
+
+pub fn config(config: &mut web::ServiceConfig) {
+    config.service(
+        web::scope("/auth")
+    );
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,2 +1,3 @@
 pub mod handlers;
 pub mod home_routes;
+pub mod auth_routes;
