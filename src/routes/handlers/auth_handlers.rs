4b5c57befca8fbc2c3a8e58a9f698ceef06c10c1
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-06-01 11:26:07 +0530

    Using Argon2 to securely store passwords

--- src/routes/handlers/auth_handlers.rs
+++ src/routes/handlers/auth_handlers.rs
@@ -1,6 +1,11 @@
 use actix_web::{Responder, post, web};
-use sea_orm::{ActiveModelTrait, ColumnTrait, Condition, EntityTrait, QueryFilter, Set};
+use sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter, Set};
 use serde::{Deserialize, Serialize};
+
+use argon2::{
+    Argon2,
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng},
+};
 
 use crate::utils::{api_response, app_state};
 
@@ -25,7 +30,7 @@ pub async fn register(
     let user_model = entity::user::ActiveModel {
         name: Set(register_json.name.clone()),
         email: Set(register_json.email.clone()),
-        password: Set(register_json.password.clone()),
+        password: Set(secure_hash(register_json.password.clone())),
         ..Default::default()
     }
     .insert(&app_state.db)
@@ -44,11 +49,7 @@ pub async fn login(
     login_json: web::Json<LoginModel>,
 ) -> impl Responder {
     let user = entity::user::Entity::find()
-        .filter(
-            Condition::all()
-                .add(entity::user::Column::Email.eq(&login_json.email))
-                .add(entity::user::Column::Password.eq(&login_json.password)),
-        )
+        .filter(entity::user::Column::Email.eq(&login_json.email))
         .one(&app_state.db)
         .await
         .unwrap();
@@ -57,5 +58,32 @@ pub async fn login(
         return api_response::ApiResponse::new(401, "User not found".to_string());
     }
 
-    api_response::ApiResponse::new(200, user.unwrap().name)
+    let user = user.unwrap();
+    match verify_hash(login_json.password.clone(), user.password) {
+        false => api_response::ApiResponse::new(401, "Wrong Password".to_string()),
+        true => api_response::ApiResponse::new(200, user.name),
+    }
+}
+
+/// Securely hash the text using Argon2 default methods and return the PHC String of the new hash
+pub(crate) fn secure_hash(text: String) -> String {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+
+    argon2
+        .hash_password(&text.into_bytes(), &salt)
+        .unwrap()
+        .to_string()
+}
+
+/// Verify the text corresponds to the hash. If it corresponds then return `true` otherwise `false`
+pub(crate) fn verify_hash(text: String, hash: String) -> bool {
+    let parsed_hash = match PasswordHash::new(&hash) {
+        Ok(hash) => hash,
+        Err(_) => return false,
+    };
+
+    Argon2::default()
+        .verify_password(&text.into_bytes(), &parsed_hash)
+        .is_ok()
 }
