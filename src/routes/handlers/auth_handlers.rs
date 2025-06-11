14fd6e5f960da0ba1fde916708c0a681ae0692d2
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-06-11 21:16:37 +0530

    Error handling for login, register route and verify_hash and secure_hash function

--- src/routes/handlers/auth_handlers.rs
+++ src/routes/handlers/auth_handlers.rs
@@ -1,4 +1,4 @@
-use actix_web::{Responder, post, web};
+use actix_web::{post, web};
 use sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter, Set};
 use serde::{Deserialize, Serialize};
 
@@ -7,10 +7,9 @@ use argon2::{
     password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng},
 };
 
-use crate::utils::{
-    api_response::{self, ApiResponse},
-    app_state,
-    jwt::encode_jwt,
+use crate::{
+    error::MainError,
+    utils::{api_response::ApiResponse, app_state, jwt::encode_jwt},
 };
 
 #[derive(Serialize, Deserialize)]
@@ -31,10 +30,13 @@ pub async fn register(
     app_state: web::Data<app_state::AppState>,
     register_json: web::Json<RegisterModel>,
 ) -> Result<ApiResponse, ApiResponse> {
+    let hash = secure_hash(register_json.password.clone())
+        .map_err(|err| ApiResponse::new(500, format!("Hashing Failed. Details: {}", err)))?;
+
     let user_model = entity::user::ActiveModel {
         name: Set(register_json.name.clone()),
         email: Set(register_json.email.clone()),
-        password: Set(secure_hash(register_json.password.clone())),
+        password: Set(hash),
         ..Default::default()
     }
     .insert(&app_state.db)
@@ -59,37 +61,37 @@ pub async fn login(
         .map_err(|err| ApiResponse::new(500, err.to_string()))?
         .ok_or(ApiResponse::new(404, "User not found".to_string()))?;
 
-    if !verify_hash(login_json.password.clone(), &user.password) {
-        return Ok(ApiResponse::new(
-            401,
-            "Unauthorized. Wrong Password.".to_string(),
-        ));
-    }
+    verify_hash(login_json.password.clone(), &user.password)
+        .map_err(|err| ApiResponse::new(401, format!("Unauthorized. Wrong Password. {}", err)))?;
 
-    let jwt = encode_jwt(user.email, user.id).unwrap();
+    let jwt =
+        encode_jwt(user.email, user.id).map_err(|err| ApiResponse::new(500, err.to_string()))?;
 
     Ok(ApiResponse::new(200, format!("{{ 'token': '{}'}}", jwt)))
 }
 
 /// Securely hash the text using Argon2 default methods and return the PHC String of the new hash
-pub(crate) fn secure_hash(text: String) -> String {
+pub(crate) fn secure_hash(text: String) -> Result<String, MainError> {
     let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
 
     argon2
         .hash_password(&text.into_bytes(), &salt)
-        .unwrap()
-        .to_string()
+        .map(|hash| hash.to_string())
+        .map_err(|err| MainError {
+            message: err.to_string(),
+        })
 }
 
 /// Verify the text corresponds to the hash. If it corresponds then return `true` otherwise `false`
-pub(crate) fn verify_hash(text: String, hash: &str) -> bool {
-    let parsed_hash = match PasswordHash::new(hash) {
-        Ok(hash) => hash,
-        Err(_) => return false,
-    };
+pub(crate) fn verify_hash(text: String, hash: &str) -> Result<(), MainError> {
+    let parsed_hash = PasswordHash::new(hash).map_err(|err| MainError {
+        message: err.to_string(),
+    })?;
 
     Argon2::default()
         .verify_password(&text.into_bytes(), &parsed_hash)
-        .is_ok()
+        .map_err(|err| MainError {
+            message: err.to_string(),
+        })
 }
